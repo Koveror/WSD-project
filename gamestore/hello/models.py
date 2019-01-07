e32519f80711860f6157ecb810577b1434f0415b
Virva Norja <virva.norja@aalto.fi>
2019-01-07 14:34:48 +0200

    added models and did migrations

--- gamestore/hello/models.py
+++ gamestore/hello/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Game(models.Model):
+    gameid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    developerid = models.ForeignKey(User, on_delete= models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    numberSod = models.IntegerField()
+    genre = models.CharField(max_length=200)
+    dateCreated = models.DateTimeField()
+
+class Score(models.Model):
+    userid = models.ForeignKey(User, on_delete= models.CASCADE)
+    gameid = models.ForeignKey(Game, on_delete= models.CASCADE)
+    score = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+class GameState(models.Model):
+    gameid = models.ForeignKey(Game, on_delete= models.CASCADE)
+    userid = models.ForeignKey(User, on_delete= models.CASCADE)
+    gameState = models.TextField()
+    timestamp = models.DateTimeField()
+
+class Purchases(models.Model):
+    gameid = models.ForeignKey(Game, on_delete= models.CASCADE)
+    userid = models.ForeignKey(User, on_delete= models.CASCADE)
