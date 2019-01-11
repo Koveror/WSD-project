758837efcd5c9592287635a2a3cd6693cf8b64ff
Niko Aarnio <niko.aarnio@aalto.fi>
2019-01-11 13:39:25 +0200

    Added example views

--- gamestore/hello/urls.py
+++ gamestore/hello/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('gamelist', views.gamelist, name='gamelist'),
+    path('purchases', views.purchases, name='purchases'),
+    path('gamestate', views.gamestate, name='gamestate')
 ]
--- gamestore/hello/views.py
+++ gamestore/hello/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
+from .models import Game, Score, GameState, Purchases
 
 def index(request):
     return HttpResponse("Hello, world. You're at the hello index.")
+
+def home(request):
+    return HttpResponse("Hello, this is home page.")
+
+def gamelist(request):
+    games = Game.objects.all()
+    str = ''
+    for game in games:
+        str = str + game.name + ' '
+    return HttpResponse(str)
+
+def score(request, gameId, userId):
+    scoreObject = Score.objects.get(gameid = gameId, userid = userId)
+    return HttpResponse(scoreObject.score)
+
+def purchases(request, userId):
+    usersPurchases = Purchases.objects.filter(userid = userId)
+    str = ''
+    for buy in purchases:
+        str = str + buy.gameid + ' '
+    return HttpResponse(str)
+
+def gamestate(request, userId, gameId):
+    state = GameState.objects.get(gameid = gameId, userid = userId)
+    return HtppResponse(state.gameState)
