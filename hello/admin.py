9704bfde03c3b8625ce9d9cf8ea1578a46596d33
Risto Koverola <risto.koverola@aalto.fi>
2019-01-14 12:57:25 +0200

    Register models to admin interface

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game, GameState, Score, Purchases
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(GameState)
+admin.site.register(Score)
+admin.site.register(Purchases)
