df5249768213ca74b3827bfbc4fbe03131fa8b7d
Virva Norja <virva.norja@aalto.fi>
2019-01-18 10:52:12 +0200

    added validators

--- hello/models.py
+++ hello/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.core.exceptions import ValidationError
+
+def validate_positive(value):
+    if (value < 0) :
+        raise ValidationError('%s Value less than zero' % value)
 
 
 class Game(models.Model):
+
     gameid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     developerid = models.ForeignKey(User, on_delete= models.CASCADE)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
-    numberSold = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=5,validators=[validate_positive])
+    numberSold = models.IntegerField(default=0,validators=[validate_positive])
     genre = models.CharField(max_length=200)
     dateCreated = models.DateTimeField()
     def __str__(self):
@@ -17,7 +23,7 @@ class Game(models.Model):
 class Score(models.Model):
     userid = models.ForeignKey(User, on_delete= models.CASCADE)
     gameid = models.ForeignKey(Game, on_delete= models.CASCADE)
-    score = models.IntegerField()
+    score = models.IntegerField(validators=[validate_positive])
     timestamp = models.DateTimeField()
     def __str__(self):
         return '{}, {}, {}'.format(self.gameid, self.userid, self.score)
